8bd762aee4a693d737430020f5abe0306a7380e8
Lana <lana.anichkova@icloud.com>
2022-11-19 20:19:10 +0400

    Fix layout structure

--- src/Components/Pages/Main/index.js
+++ src/Components/Pages/Main/index.js
@@ -1,7 +1,6 @@
 import Contacts from './Contacts'
-import Footer from './Footer'
 import Statistics from './Statistics'
 import Steps from './Steps'
 import WhySelectUs from './WhySelectUs'
 
-export { Contacts, Footer, Statistics, Steps, WhySelectUs }
+export { Contacts, Statistics, Steps, WhySelectUs }
