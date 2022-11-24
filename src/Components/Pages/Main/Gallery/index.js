8aff7170d144be06a1664d5c9d834f6e2135080b
Lana <lana.anichkova@icloud.com>
2022-11-24 21:47:13 +0400

    Add gallery block

---
+++ src/Components/Pages/Main/Gallery/index.js
@@ -0,0 +1,117 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+
+import { mockGalleryPreviewImage } from 'Assets/Images'
+import { Button, Text } from 'Components/UI'
+import { MapPin } from 'phosphor-react'
+
+import {
+  CaptionHolder,
+  Card,
+  CardHolder,
+  CardsHolder,
+  Container,
+  Content,
+  StyledImage,
+} from './styles'
+
+const ICON_WEIGHT = 'bold'
+
+function Gallery() {
+  const { t } = useTranslation('translation', {
+    keyPrefix: 'pages.main.gallery',
+  })
+
+  const CARDS = [
+    {
+      image: mockGalleryPreviewImage,
+      text: t('s1text'),
+      subtext: t('s2text'),
+    },
+    {
+      image: mockGalleryPreviewImage,
+      text: t('s1text'),
+      subtext: t('s2text'),
+    },
+    {
+      image: mockGalleryPreviewImage,
+      text: t('s1text'),
+      subtext: t('s2text'),
+    },
+    {
+      image: mockGalleryPreviewImage,
+      text: t('s1text'),
+      subtext: t('s2text'),
+    },
+    {
+      image: mockGalleryPreviewImage,
+      text: t('s1text'),
+      subtext: t('s2text'),
+    },
+    {
+      image: mockGalleryPreviewImage,
+      text: t('s1text'),
+      subtext: t('s2text'),
+    },
+    {
+      image: mockGalleryPreviewImage,
+      text: t('s1text'),
+      subtext: t('s2text'),
+    },
+    {
+      image: mockGalleryPreviewImage,
+      text: t('s1text'),
+      subtext: t('s2text'),
+    },
+    {
+      image: mockGalleryPreviewImage,
+      text: t('s1text'),
+      subtext: t('s2text'),
+    },
+    {
+      image: mockGalleryPreviewImage,
+      text: t('s1text'),
+      subtext: t('s2text'),
+    },
+  ]
+
+  return (
+    <Container>
+      <Content>
+        <Text h3 heading mb={3}>
+          {t('title')}
+        </Text>
+        <Text body caption1 mb={2}>
+          {t('1subTitle')}
+        </Text>
+        <Text body caption1 mb={10}>
+          {t('2subTitle')}
+        </Text>
+        <Button big tertery>
+          <MapPin weight={ICON_WEIGHT} />
+          Объекты на карте
+        </Button>
+        <CardsHolder>
+          {CARDS.map(card => (
+            <CardHolder key={card.subtext}>
+              <Card>
+                <StyledImage src={card.image.src} />
+                <CaptionHolder>
+                  <Text heading mb={2} subHeader3>
+                    {card.text}
+                  </Text>
+                  <Text caption3 muted>
+                    {card.subtext}
+                  </Text>
+                </CaptionHolder>
+              </Card>
+            </CardHolder>
+          ))}
+        </CardsHolder>
+        <Button big>Показать все</Button>
+      </Content>
+    </Container>
+  )
+}
+
+export default Gallery
---
+++ src/Components/Pages/Main/Gallery/styles.js
@@ -0,0 +1,90 @@
+import styled from 'styled-components'
+import themeGet from '@styled-system/theme-get'
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-self: flex-start;
+  width: 100%;
+  background-color: ${themeGet('colors.bg.primary')};
+`
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  padding-top: 80px;
+  width: 100%;
+
+  button {
+    border-radius: 0;
+  }
+`
+export const CardsHolder = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  grid-template-columns: repeat(4, 1fr);
+  width: 100%;
+`
+
+export const CardHolder = styled.div`
+  position: relative;
+  height: 320px;
+  width: 20vw;
+`
+
+export const Card = styled.button`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  cursor: pointer;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  transition: all ${themeGet('transitionTime.long')};
+
+  p {
+    transition: opacity ${themeGet('transitionTime.long')} ease-in;
+    opacity: 0;
+  }
+
+  &:hover {
+    z-index: 1;
+    box-shadow: ${themeGet('shadow.large')};
+
+    img {
+      filter: grayscale(0);
+    }
+
+    div {
+      height: 96px;
+    }
+
+    p {
+      opacity: 1;
+    }
+  }
+`
+
+export const StyledImage = styled.img`
+  width: 100%;
+  height: 320px;
+  object-fit: cover;
+  filter: grayscale(100%);
+  transition: all ${themeGet('transitionTime.long')};
+`
+export const CaptionHolder = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  overflow: hidden;
+  height: 0;
+  transition: height ${themeGet('transitionTime.long')};
+  background-color: ${themeGet('colors.bg.default')};
+`
