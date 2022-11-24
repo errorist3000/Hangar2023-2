6eb63841fab0ab2924e42c10f9c5b5df8bbf5894
Lana <lana.anichkova@icloud.com>
2022-11-24 21:55:29 +0400

    Add new sections

--- src/Components/Pages/Main/index.js
+++ src/Components/Pages/Main/index.js
@@ -1,6 +1,9 @@
 import Contacts from './Contacts'
+import Gallery from './Gallery'
+import Hero from './Hero'
+import OurNews from './OurNews'
 import Statistics from './Statistics'
 import Steps from './Steps'
 import WhySelectUs from './WhySelectUs'
 
-export { Contacts, Statistics, Steps, WhySelectUs }
+export { Contacts, Gallery, Hero, OurNews, Statistics, Steps, WhySelectUs }
