df45e6577e8039bf2557efcef539f87eb1d6c3f9
Lana <lana.anichkova@icloud.com>
2022-11-26 23:02:20 +0400

    Update assets

--- src/Assets/Images/index.js
+++ src/Assets/Images/index.js
@@ -1,5 +1,11 @@
 import statisticsBgImage from './bgImage.jpg'
 import partnershipImage from './partnership.jpg'
+import OurNewsPrewiewImage from './preview_005_1x.webp'
 import mockGalleryPreviewImage from './preview_006_2x.webp'
 
-export { mockGalleryPreviewImage, partnershipImage, statisticsBgImage }
+export {
+  mockGalleryPreviewImage,
+  OurNewsPrewiewImage,
+  partnershipImage,
+  statisticsBgImage,
+}
