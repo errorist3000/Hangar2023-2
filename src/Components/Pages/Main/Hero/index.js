ca4d13bbed10eb704d75a6b673d8a045fcd4e0f7
Lana <lana.anichkova@icloud.com>
2022-12-09 18:36:58 +0400

    Add hero slide animation

--- src/Components/Pages/Main/Hero/index.js
+++ src/Components/Pages/Main/Hero/index.js
@@ -1,38 +1,70 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import PropTypes from 'prop-types'
 
-import { Button, Column, Row, Text } from 'Components/UI'
+import { Button, Column, Image, Row, Text } from 'Components/UI'
 import { LANDING_SECTION_ID } from 'Constants/ids'
+import { Autoplay } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
 
-import { Container, Content } from './styles'
+import { orderBy } from 'lodash/collection'
 
-function Hero() {
+import { Container, Content, PictureOverlay } from './styles'
+
+import 'swiper/swiper.min.css'
+
+function Hero({ data }) {
+  const sortedDataByOrder = useMemo(() => orderBy(data, 'order', 'asc'), [data])
+
   return (
     <Container id={LANDING_SECTION_ID.hero}>
-      <Content>
-        <Column>
-          <Text h2 inverse mb={10}>
-            Тентовые каркасные здания
-          </Text>
-          <Text action1 inverse mb={6}>
-            Проектирование, производство и монтаж каркасных тентовых ангаров,
-            павильонов, тепляков и укрытий
-          </Text>
-          <Text action1 inverse mb={14}>
-            Вы можете заполнить и отправить форму запроса. Мы вышлем
-            коммерческое предложение
-          </Text>
-          <Row gap={32} justifyCenter>
-            <Button big outline width={192}>
-              Запрос
-            </Button>
-            <Button big width={192}>
-              Калькулятор
-            </Button>
-          </Row>
-        </Column>
-      </Content>
+      <Swiper
+        autoplay={{
+          delay: 5000,
+          pauseOnMouseEnter: true,
+          disableOnInteraction: false,
+        }}
+        loop
+        modules={[Autoplay]}
+        speed={2000}
+      >
+        {sortedDataByOrder.map(slide => (
+          <SwiperSlide key={slide.order}>
+            <Image data={slide.image} />
+            <PictureOverlay />
+            <Content>
+              <Column>
+                <Text h2 inverse mb={10}>
+                  {slide.title}
+                </Text>
+                <Text action1 inverse mb={6}>
+                  {slide.subtitle}
+                </Text>
+                <Text action1 inverse mb={14}>
+                  {slide.text}
+                </Text>
+                <Row gap={32} justifyCenter>
+                  <Button big outline width={192}>
+                    Запрос
+                  </Button>
+                  <Button big width={192}>
+                    Калькулятор
+                  </Button>
+                </Row>
+              </Column>
+            </Content>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </Container>
   )
+}
+
+Hero.defaultProps = {
+  data: [],
+}
+
+Hero.propTypes = {
+  data: PropTypes.array,
 }
 
 export default Hero
