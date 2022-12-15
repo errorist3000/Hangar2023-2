9f206d087c47cbd7f8262c2d37b9f6e01dc8dc9c
Lana <lana.anichkova@icloud.com>
2022-12-15 20:49:37 +0400

    Fix styles modal

--- src/Components/UI/Modal/styles.js
+++ src/Components/UI/Modal/styles.js
@@ -7,8 +7,7 @@ export const StyledModal = styled(Modal)`
   display: flex;
   flex-direction: column;
   border-radius: 12px;
-  min-width: 480px;
-  min-height: 300px;
   background-color: ${themeGet('colors.bg.default')};
   box-shadow: ${themeGet('shadow.large')};
+  position: relative;
 `
