2832f51f529adadc47fda3b39e5fd00cb410ba7f
Lana <lana.anichkova@icloud.com>
2022-12-15 20:50:32 +0400

    Fix styles image

--- src/Components/UI/Image/index.js
+++ src/Components/UI/Image/index.js
@@ -1,24 +1,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+const imageCoverCss = ({ cover }) =>
+  cover &&
+  css`
+    object-fit: cover;
+  `
 
 const BaseImage = styled.img`
   width: 100%;
   height: 100%;
+
+  ${imageCoverCss}
 `
 
-function Image({ data }) {
+function Image({ data, cover }) {
   return (
     <BaseImage
       alt={data?.alt}
+      cover={cover}
       src={data?.jpeg}
       srcSet={`${data?.webp1x} 1x,${data?.webp2x} 2x`}
     />
   )
 }
 
+Image.defaultProps = {
+  cover: false,
+}
+
 Image.propTypes = {
+  cover: PropTypes.bool,
   data: PropTypes.object.isRequired,
 }
 
