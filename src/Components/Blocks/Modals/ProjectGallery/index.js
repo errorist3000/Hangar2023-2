8884a2dad8b4965c70479355dd80aedbc7144bd1
Lana <lana.anichkova@icloud.com>
2022-12-15 21:00:10 +0400

    Add Swiper, SwiperHolder (Navigation, Pagination) and MapHolder

--- src/Components/Blocks/Modals/ProjectGallery/index.js
+++ src/Components/Blocks/Modals/ProjectGallery/index.js
@@ -1,37 +1,87 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 import { useQuery } from '@apollo/client'
-import { ClientOnly, Modal, Text } from 'Components/UI'
-import { MapPin, X } from 'phosphor-react'
+import { ClientOnly, Image, Loader, Modal, Text } from 'Components/UI'
+import { CaretLeft, CaretRight, MapPin, X } from 'phosphor-react'
 import projectQuery from 'Services/DatoCMS/Queries/project.graphql'
+import { Navigation, Pagination } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
 
 import noop from 'lodash/noop'
 
-import { Header, StyledButton } from './styles'
+import {
+  Header,
+  MapHolder,
+  NavigationButton,
+  StyledButton,
+  SwiperHolder,
+} from './styles'
+
+import 'swiper/swiper.min.css'
 
 function ProjectGallery({ isOpen, projectId, onClose }) {
-  const { data } = useQuery(projectQuery, {
+  const [isMap, setIsMap] = useState(false)
+
+  const { data, loading } = useQuery(projectQuery, {
     variables: { id: { in: projectId } },
     skip: !projectId,
   })
 
   const projectData = data?.project
 
+  console.log(isMap)
+
   return (
     <ClientOnly>
-      <Modal isCustom isOpen={isOpen} onClose={onClose}>
-        <Header>
-          <StyledButton onClick={onClose}>
-            <MapPin size={24} />
+      <Modal isCustom isOpen={isOpen}>
+        <Header relative={loading}>
+          <StyledButton onClick={() => setIsMap(true)}>
+            {!isMap && <MapPin size={24} />}
           </StyledButton>
           <Text inverse subHeader1>
             {projectData?.title}
           </Text>
-          <StyledButton>
+          <StyledButton onClick={isMap ? () => setIsMap(false) : onClose}>
             <X size={24} />
           </StyledButton>
         </Header>
+
+        {isMap ? (
+          <MapHolder>Map</MapHolder>
+        ) : (
+          <SwiperHolder low={loading}>
+            {loading ? (
+              <Loader />
+            ) : (
+              <Swiper
+                grabCursor
+                loop
+                modules={[Navigation, Pagination]}
+                navigation={{
+                  nextEl: '.swiper-button-next',
+                  prevEl: '.swiper-button-prev',
+                }}
+                pagination={{
+                  bulletActiveClass: 'swiper-pagination-bullet-active',
+                  bulletClass: 'swiper-pagination-bullet',
+                }}
+              >
+                {projectData?.imageSet?.map(item => (
+                  <SwiperSlide key={item.id}>
+                    <Image cover data={item} />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            )}
+            <NavigationButton className="swiper-button-prev">
+              <CaretLeft size={80} />
+            </NavigationButton>
+            <NavigationButton className="swiper-button-next">
+              <CaretRight size={80} />
+            </NavigationButton>
+          </SwiperHolder>
+        )}
       </Modal>
     </ClientOnly>
   )
--- src/Components/Blocks/Modals/ProjectGallery/styles.js
+++ src/Components/Blocks/Modals/ProjectGallery/styles.js
@@ -1,29 +1,106 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import themeGet from '@styled-system/theme-get'
 
+const headerRelativeCss = ({ relative }) =>
+  relative &&
+  css`
+    position: relative;
+  `
+
 export const Header = styled.div`
-  position: relative;
+  position: absolute;
   display: flex;
   justify-content: space-between;
+  text-align: center;
   align-items: center;
-  background-color: ${themeGet('colors.bg.tertiary')};
-  max-height: 50px;
-  max-width: 1170px;
+  background-color: ${themeGet('colors.bg.tertiary')}cc;
+  width: 100%;
+  min-height: 50px;
   z-index: ${themeGet('zIndexes.landingHeader')};
   padding: 8px 12px;
-  opacity: 0.6;
   border-top-left-radius: inherit;
   border-top-right-radius: inherit;
+
+  ${headerRelativeCss}
+  }
 `
 
 export const StyledButton = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
+  min-width: 24px;
   cursor: pointer;
   color: ${themeGet('colors.text.inverse')};
 
   &:hover {
     color: ${themeGet('colors.primary')};
   }
+`
+
+export const NavigationButton = styled.button`
+  position: absolute;
+  display: flex;
+  justify-items: center;
+  align-items: center;
+  color: ${themeGet('colors.text.inverse')};
+  cursor: pointer;
+  z-index: 9999;
+`
+
+const swiperHolderLowCss = ({ low }) =>
+  low &&
+  css`
+    max-height: 640px;
+  `
+const holderCss = css`
+  max-width: 1170px;
+  max-height: 690px;
+  width: 90vw;
+  height: 90vh;
+  border-radius: inherit;
+`
+
+export const SwiperHolder = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .swiper {
+    border-radius: inherit;
+    height: 100%;
+  }
+
+  .swiper-button-prev {
+    left: 0;
+  }
+
+  .swiper-button-next {
+    right: 0;
+  }
+
+  .swiper-pagination {
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    bottom: 20px;
+    gap: ${themeGet('space.3')}px;
+    z-index: 9999;
+  }
+
+  > img {
+    object-fit: cover;
+  }
+
+  ${holderCss}
+  ${swiperHolderLowCss}
+`
+
+export const MapHolder = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  ${holderCss}
 `
