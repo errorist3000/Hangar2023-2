15e93e00d94b1618e0ff1a5482984b4a90fb463d
Lana <lana.anichkova@icloud.com>
2022-12-15 20:46:23 +0400

    Add swiper

--- src/Themes/globalStyle.js
+++ src/Themes/globalStyle.js
@@ -3,9 +3,11 @@ import styledNormalize from 'styled-normalize'
 
 import globals from 'Themes/Styles/globals'
 import modal from 'Themes/Styles/modal'
+import swiper from 'Themes/Styles/swiper'
 
 export default createGlobalStyle`
   ${styledNormalize}
   ${globals}
   ${modal}
+  ${swiper}
 `
