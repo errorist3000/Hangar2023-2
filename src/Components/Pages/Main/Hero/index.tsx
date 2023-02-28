584b3b536de61e5bdeb8b7dd6042ebfa492d91c9
Roman Zaionchkovski <oneliner@vivaldi.net>
2023-03-01 00:12:04 +0400

    Add calculator page link

--- src/Components/Pages/Main/Hero/index.tsx
+++ src/Components/Pages/Main/Hero/index.tsx
@@ -1,5 +1,6 @@
-import React, { useMemo, useState } from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 
+import { useRouter } from 'next/router'
 import { Autoplay } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
 
@@ -9,6 +10,7 @@ import { ContactUsModal } from 'Components/Blocks'
 import { Button, Column, Image, Text } from 'Components/UI'
 
 import { LandingPageSectionIds } from 'Constants/ids'
+import { CALCULATOR } from 'Constants/routes'
 
 import { ButtonHolder, Container, Content, PictureOverlay } from './styles'
 
@@ -18,9 +20,15 @@ type Props = {
   data?: DatoSchema.HeroRecord[]
 }
 function Hero({ data }: Props) {
+  const router = useRouter()
+
   const [isContactUsModalOpen, setIsContactUsModalOpen] = useState(false)
 
   const sortedDataByOrder = useMemo(() => orderBy(data, 'order', 'asc'), [data])
+
+  const handleCalculatorClick = useCallback(() => {
+    router.push(CALCULATOR).then()
+  }, [router])
 
   return (
     <Container id={LandingPageSectionIds.Hero}>
@@ -58,7 +66,7 @@ function Hero({ data }: Props) {
                   >
                     Запрос
                   </Button>
-                  <Button big width={192}>
+                  <Button big width={192} onClick={handleCalculatorClick}>
                     Калькулятор
                   </Button>
                 </ButtonHolder>
